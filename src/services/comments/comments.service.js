84c362788c7912c95cef134ed4dbcf6b5fec598b
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2022-02-12 14:57:29 +0200

    Fix comments service

--- src/services/comments/comments.service.js
+++ src/services/comments/comments.service.js
@@ -1,4 +1,4 @@
-// Initializes the `comment` service on path `/comment`
+// Initializes the `comments` service on path `/comments`
 const { Comments } = require('./comments.class');
 const createModel = require('../../models/comments.model');
 const hooks = require('./comments.hooks');
@@ -10,10 +10,10 @@ module.exports = function (app) {
   };
 
   // Initialize our service with any options it requires
-  app.use('/comment', new Comments(options, app));
+  app.use('/comments', new Comments(options, app));
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('comment');
+  const service = app.service('comments');
 
   service.hooks(hooks);
 };
