d07ce39390452bc81ac4cab00153331cb325e02c
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2022-02-13 16:57:12 +0200

    Add signup endpoint

--- src/session_authentication.js
+++ src/session_authentication.js
@@ -1,9 +1,34 @@
 const path = require('path')
 const { authenticate } = require('@feathersjs/express')
+const bcrypt = require('bcrypt')
 
 module.exports = (app) => {
   app.get('/login', (req, res) => res.sendFile(path.join(app.get('public'), 'login.html')))
   app.get('/signup', (req, res) => res.sendFile(path.join(app.get('public'), 'signup.html')))
+
+  app.post('/signup', async (req, res, next) => {
+    try {
+      const users = app.get('sequelizeClient').models.users
+      const { firstName, lastName, email, password } = req.body
+      const passwordHash = await bcrypt.hash(password, bcrypt.genSaltSync(8))
+      await users.create({ firstName, lastName, email, password: passwordHash })
+
+      const { accessToken } = await app.service('api/authentication').create({
+        strategy: 'local',
+        email,
+        password,
+      })
+
+      req.session.authentication = {
+        strategy: 'jwt',
+        accessToken,
+      }
+
+      res.redirect('/home')
+    } catch (error) {
+      next(error)
+    }
+  })
 
   app.post('/login', async (req, res, next) => {
     try {
