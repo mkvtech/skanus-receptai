1dd50c850df2657b88e257ee813705f3822fda27
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2022-02-14 14:36:51 +0200

    Add new recipe form

--- public/recipes.js
+++ public/recipes.js
@@ -1,7 +1,8 @@
 /*global document, $, axios */
 
 let jwt
-let recipesLinksContainer = $('#list-of-recipes')
+const recipesLinksContainer = $('#list-of-recipes')
+const rightSideContainer = $('#right-side-container')
 let recipes = []
 var links = document.getElementsByClassName('nav-links')
 
@@ -37,7 +38,7 @@ const fetchRecipes = async (jwt) => {
 }
 
 const renderRecipesMenu = (recipes) => {
-  recipesLinksContainer.innerHTML = ''
+  recipesLinksContainer.html('')
 
   recipes.forEach((recipe) => {
     recipesLinksContainer.append(renderRecipeLink(recipe))
@@ -47,7 +48,7 @@ const renderRecipesMenu = (recipes) => {
 const renderRecipeLink = (recipe) => {
   return `
     <li class="recipe-title">
-      <a class="recipe-link" onclick="selectRecipe(${recipe.id})">
+      <a class="recipe-link" onclick="onRecipeSelect(${recipe.id})">
         ${recipe.title}
       </a>
     </li>
@@ -55,17 +56,38 @@ const renderRecipeLink = (recipe) => {
 }
 
 // eslint-disable-next-line no-unused-vars
-const selectRecipe = (recipeId) => {
+const onRecipeSelect = (recipeId) => {
   console.log(`Selecting recipe with ID = ${recipeId}...`)
+
+  rightSideContainer.empty()
+  rightSideContainer.html(recipeView)
+
   const recipe = recipes.find(recipe => recipe.id === recipeId)
-  renderRecipe(recipe)
+
+  fillRecipeView(recipe)
+}
+
+const onNewRecipe = () => {
+  console.log('Opening form for new recipe...')
+
+  rightSideContainer.empty()
+  rightSideContainer.html(recipeForm)
+}
+
+const onRecipeEdit = () => {
+  console.log('Opening form for existing recipe...')
+
+  rightSideContainer.empty()
+  rightSideContainer.html(recipeForm)
+
+  fillRecipeForm()
 }
 
 const setHtmlMultiline = (element, multilineText) => {
   element.html(multilineText.split('\n').map(line => `<p>${line}</p>`))
 }
 
-const renderRecipe = (recipe) => {
+const fillRecipeView = (recipe) => {
   const titleHeading = $('#recipe-title')
   const ingredientsParagraph = $('#recipe-ingredients')
   const descriptionParagraph = $('#recipe-description')
@@ -74,6 +96,72 @@ const renderRecipe = (recipe) => {
   setHtmlMultiline(ingredientsParagraph, recipe.ingredients)
   setHtmlMultiline(descriptionParagraph, recipe.description)
 }
+
+// eslint-disable-next-line no-unused-vars
+const onRecipeFormSubmit = async () => {
+  const $recipeTitle = $('#recipe-form-title')
+  const $recipeIngredients = $('#recipe-form-ingredients')
+  const $recipeDescription = $('#recipe-form-description')
+
+  // front-end validation here
+
+  const data = {
+    title: $recipeTitle.val(),
+    ingredients: $recipeIngredients.val(),
+    description: $recipeDescription.val(),
+  }
+
+  const response = await axios.post('/api/recipes', data, { headers: { Authorization: `Bearer ${jwt}` } })
+  const newRecipe = response.data
+
+  recipes.push(newRecipe)
+  onRecipeSelect(newRecipe.id)
+  renderRecipesMenu(recipes)
+}
+
+const createRecipeForm = () => {
+  return $(`
+    <div id="recipe-form">
+      <header><h1 id="recipe-edit-form-title">New Recipe</h1></header>
+
+      <form action="javascript:void(0);">
+        <label for="recipe-form-title">Pabadinimas: </label>
+        <input type="text" name="title" id="recipe-form-title" required />
+        <br />
+
+        <label for="recipe-form-ingredients">Ingredientai: </label>
+        <input type="text" name="ingredients" id="recipe-form-ingredients" required />
+        <br />
+
+        <label for="recipe-form-description">Gaminimo budas: </label>
+        <input type="text" name="description" id="recipe-form-description" required />
+        <br />
+
+        <button type="submit" onclick="onRecipeFormSubmit()">Sukurti</button>
+      </form>
+    </div>
+  `)
+}
+
+const createRecipeView = () => {
+  return $(`
+    <div id="recipe-view">
+      <header><h1 id="recipe-title" class="right-header">Recepto pavadinimas</h1></header>
+
+      <h2>Ingredientai</h2>
+      <div id="recipe-ingredients" class="ingridients"></div>
+
+      <h2>Gaminimo budas</h2>
+      <div id="recipe-description" class="description"></div>
+
+      <h2>Komentarai</h2>
+      <div id="recipe-comments">(TODO)</div>
+    </div>
+  `)
+}
+
+const recipeForm = createRecipeForm()
+const recipeView = createRecipeView()
 
 ;(async () => {
   jwt = await fetchJwt()
