b1850f5aab7657ad68ca9cdf1885aac38c09088a
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2022-02-15 21:08:14 +0200

    Add and configure persistent session storage

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,5 @@
 const session = require('express-session')
+const SQLiteStore = require('connect-sqlite3')(session)
 
 module.exports = function (app) {
   // Add your custom middleware here. Remember that
@@ -9,6 +10,9 @@ module.exports = function (app) {
       secret: 'session-secret',
       saveUninitialized: false,
       resave: true,
+      cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 }, // 1 week
+      store: new SQLiteStore({ table: 'sessions', db: 'sessions.sqlite', dir: '.' }),
+      name: 'skanus-receptai-session-id'
     })
   )
 }
