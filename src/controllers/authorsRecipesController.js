4c1390ede1c08fa38090669a1e70e28c8bdde23c
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2022-03-22 23:17:44 +0200

    REC-6 Prideti receptus

--- src/controllers/authorsRecipesController.js
+++ src/controllers/authorsRecipesController.js
@@ -1,5 +1,13 @@
-module.exports = {
-    index: (req, res) => {
-      res.render('pages/authorsRecipes/index.html.ejs', {author: req.user})
-    },
-  }  
+class AuthorsRecipesController {
+  constructor(app) {
+    this.app = app
+  }
+
+  index = async (req, res) => {
+    const recipes = await this.app.get('models').recipes.findAll({ where: { userId: req.user.id } })
+
+    res.render('pages/authorsRecipes/index.html.ejs', { author: req.user, recipes })
+  }
+}
+
+module.exports = AuthorsRecipesController
--- src/router.js
+++ src/router.js
@@ -4,16 +4,20 @@ const { authenticate } = require('@feathersjs/express')
 const allowAnonymous = require('./middleware/allowAnonymous')
 const setSessionAuthentication = require('./middleware/setSessionAuthentication')
 
-const router = express.Router()
-
 const debugController = require('./controllers/debugController')
-router.get('/debug', setSessionAuthentication(), allowAnonymous(), authenticate('jwt', 'anonymous'), debugController.show)
-router.get('/debug_protected', setSessionAuthentication(), authenticate('jwt'), debugController.show)
-
 const applicationController = require('./controllers/applicationController')
-router.get('/', setSessionAuthentication(), allowAnonymous(), authenticate('jwt', 'anonymous'), applicationController.index)
+const AuthorsRecipesController = require('./controllers/authorsRecipesController')
 
-const authorsRecipesController = require('./controllers/authorsRecipesController')
-router.get('/author', setSessionAuthentication(), allowAnonymous(), authenticate('jwt', 'anonymous'), authorsRecipesController.index)
+module.exports = (app) => {
+  const router = express.Router()
 
-module.exports = router
+  router.get('/debug', setSessionAuthentication(), allowAnonymous(), authenticate('jwt', 'anonymous'), debugController.show)
+  router.get('/debug_protected', setSessionAuthentication(), authenticate('jwt'), debugController.show)
+
+  router.get('/', setSessionAuthentication(), allowAnonymous(), authenticate('jwt', 'anonymous'), applicationController.index)
+
+  const authorsRecipesController = new AuthorsRecipesController(app)
+  router.get('/author', setSessionAuthentication(), allowAnonymous(), authenticate('jwt', 'anonymous'), authorsRecipesController.index)
+
+  return router
+}
