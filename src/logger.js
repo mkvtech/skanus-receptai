5154ab5659e1e315cb97e0e1c18f888ac338f063
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2022-03-28 21:06:29 +0300

    Change logger to output more info about errors

--- src/logger.js
+++ src/logger.js
@@ -2,12 +2,13 @@ const { createLogger, format, transports } = require('winston')
 
 // Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
 const logger = createLogger({
-  level: 'info',
-  format: format.combine(format.splat(), format.simple()),
+  level: 'debug',
 
-  // More detailed output:
-  // level: 'debug',
-  // format: format.combine(format.errors({ stack: true }), format.timestamp(), format.prettyPrint()),
+  format: format.combine(
+    format.errors({ stack: true }),
+    format.timestamp(),
+    format.prettyPrint(),
+  ),
 
   transports: [new transports.Console()],
 })
