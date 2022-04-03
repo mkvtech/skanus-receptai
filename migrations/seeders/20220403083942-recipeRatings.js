0ac2c8f4e98004508b28bc1eb5cce720a55a6070
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2022-04-03 21:43:36 +0300

    REC-12 Pridėjau pavyzdinius duomenis

---
+++ migrations/seeders/20220403083942-recipeRatings.js
@@ -0,0 +1,115 @@
+'use strict'
+
+const { recipe_ratings } = require('../models')
+
+module.exports = {
+  // eslint-disable-next-line no-unused-vars
+  async up (queryInterface, Sequelize) {
+    await recipe_ratings.bulkCreate([
+      // userId: 1 recipe_ratings
+      {
+        id: 1,
+        userId: 1,
+        recipeId: 2,
+        rating: 5,
+      },
+      {
+        id: 2,
+        userId: 1,
+        recipeId: 3,
+        rating: 4,
+      },
+      {
+        id: 3,
+        userId: 1,
+        recipeId: 4,
+        rating: 1,
+      },
+
+      // userId: 2 recipe_ratings
+      {
+        id: 4,
+        userId: 2,
+        recipeId: 1,
+        rating: 2,
+      },
+      {
+        id: 5,
+        userId: 2,
+        recipeId: 3,
+        rating: 3,
+      },
+      {
+        id: 6,
+        userId: 2,
+        recipeId: 4,
+        rating: 2,
+      },
+
+      // userId: 3 recipe_ratings
+      {
+        id: 7,
+        userId: 3,
+        recipeId: 1,
+        rating: 5,
+      },
+
+      // userId: 4 recipe_ratings
+      {
+        id: 8,
+        userId: 4,
+        recipeId: 1,
+        rating: 5,
+      },
+      {
+        id: 9,
+        userId: 4,
+        recipeId: 2,
+        rating: 5,
+      },
+      {
+        id: 10,
+        userId: 4,
+        recipeId: 3,
+        rating: 5,
+      },
+      {
+        id: 11,
+        userId: 4,
+        recipeId: 4,
+        rating: 5,
+      },
+
+      // userId: 5 recipe_ratings
+      {
+        id: 12,
+        userId: 5,
+        recipeId: 1,
+        rating: 1,
+      },
+      {
+        id: 13,
+        userId: 5,
+        recipeId: 2,
+        rating: 2,
+      },
+      {
+        id: 14,
+        userId: 5,
+        recipeId: 3,
+        rating: 3,
+      },
+      {
+        id: 15,
+        userId: 5,
+        recipeId: 4,
+        rating: 4,
+      },
+    ])
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('recipe_ratings', null, {})
+  }
+}
