d59b24d5200f63673c853924f5cdec2858b8a44e
Yoleeee <vrulevich@gmail.com>
2022-04-06 03:19:11 +0300

    REC-6 Paspaudus ant vartotojo vardo prie recepto galima pamatyti jo sukurtus receptus

--- src/controllers/authorsRecipesController.js
+++ src/controllers/authorsRecipesController.js
@@ -1,12 +1,48 @@
-class AuthorsRecipesController {
+const BaseController = require("./baseController")
+
+class AuthorsRecipesController extends BaseController {
   constructor(app) {
-    this.app = app
+    super(app)
   }
 
   index = async (req, res) => {
-    const recipes = await this.app.get('models').recipes.findAll({ where: { userId: req.user.id } })
+    const recipes = await this.models.recipes.findAll({
+      where: {
+        id: req.params.id,
+      },
+      include: this.models.users,
+    })
 
-    res.render('pages/authorsRecipes/index.html.ejs', { author: req.user, recipes })
+    const author = await this.models.users.findOne({
+      where: {
+        id: req.params.id,
+      },
+    })
+
+    res.render('pages/authorsRecipes/index.html.ejs', {
+      recipes,
+      author,
+      context: await this.viewContext(req),
+    })
+  }
+
+  show = async (req, res) => {
+    const recipe = await this.models.recipes.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: this.models.users
+    })
+
+    if (recipe) {
+      res.render('pages/authorsRicpes/index.html.ejs', {
+        recipe,
+        recipesUrl: `${this.utils.fullBaseUrl}/authorsrecipes`,
+        context: await this.viewContext(req),
+      })
+    } else {
+      res.sendStatus(404)
+    }
   }
 }
 
