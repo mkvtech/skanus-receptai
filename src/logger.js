d4347fc97210d67a834b99236b7d6ae6e8311a37
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2022-04-12 23:38:00 +0300

    Skip 4** logs in test mode

--- src/logger.js
+++ src/logger.js
@@ -10,11 +10,24 @@ const removeFields = format((info, opts) => {
   return info
 })
 
+const skipLogWithCodes = format((info, opts) => {
+  if (info.code && Array.isArray(opts)) {
+    if (opts.some((codeRegexp) => codeRegexp.test('' + info.code))) {
+      return null
+    }
+  }
+
+  return info
+})
+
 // Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
 const logger = createLogger({
   level: 'debug',
 
-  format: format.combine(
+  format: format.combine(...[
+
+    // When in test mode, skip 401 and 404 logs
+    ...process.env.NODE_ENV === 'test' ? [skipLogWithCodes([/4../])] : [],
 
     // Feathersjs 'hook' field in log has too much data to log
     removeFields(['hook']),
@@ -23,7 +36,7 @@ const logger = createLogger({
     // format.colorize(), // TODO: Colorize output
     format.timestamp(),
     format.prettyPrint(),
-  ),
+  ]),
 
   transports: [new transports.Console()],
 })
