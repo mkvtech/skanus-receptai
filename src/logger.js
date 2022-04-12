bd8085452e76997075a7d4cb1f34b609f3b1f626
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2022-04-12 19:54:34 +0300

    Fix logger (again)

--- src/logger.js
+++ src/logger.js
@@ -1,14 +1,28 @@
 const { createLogger, format, transports } = require('winston')
 
+const removeFields = format((info, opts) => {
+  if (opts) {
+    opts.forEach(field => {
+      delete info[field]
+    })
+  }
+
+  return info
+})
+
 // Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
 const logger = createLogger({
   level: 'debug',
 
   format: format.combine(
+
+    // Feathersjs 'hook' field in log has too much data to log
+    removeFields(['hook']),
+
     format.errors({ stack: true }),
-    format.colorize(),
+    // format.colorize(), // TODO: Colorize output
     format.timestamp(),
-    format.simple(),
+    format.prettyPrint(),
   ),
 
   transports: [new transports.Console()],
