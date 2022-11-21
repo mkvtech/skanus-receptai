6d28b0b4ccb3d91be79220a25025ff63631d35b6
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2022-11-21 15:34:32 +0200

    fix

--- src/session_authentication.js
+++ src/session_authentication.js
@@ -2,21 +2,32 @@ const path = require('path')
 const { authenticate } = require('@feathersjs/express')
 const bcrypt = require('bcrypt')
 
-module.exports = (a) => {
-  const afterLoginRedirect = a.get('other').afterLoginRedirect
-  const afterSignupRedirect = a.get('other').afterSignupRedirect
+const BCRYPT_GEN_SALT = 8
 
-  a.get('/login', (req, res) => res.sendFile(path.join(a.get('public'), 'login.html')))
-  a.get('/signup', (req, res) => res.sendFile(path.join(a.get('public'), 'signup.html')))
+function encryptPassword(password) {
+  return bcrypt.hash(password, bcrypt.genSaltSync(BCRYPT_GEN_SALT))
+}
 
-  a.post('/signup', async (req, res, next) => {
+function setAuthenticationFromSession(req, res, next) {
+  req.authentication = req.session.authentication
+  next()
+}
+
+module.exports = (app) => {
+  const afterLoginRedirect = app.get('other').afterLoginRedirect
+  const afterSignupRedirect = app.get('other').afterSignupRedirect
+
+  app.get('/login', (req, res) => res.sendFile(path.join(app.get('public'), 'login.html')))
+  app.get('/signup', (req, res) => res.sendFile(path.join(app.get('public'), 'signup.html')))
+
+  app.post('/signup', async (req, res, next) => {
     try {
-      const users = a.get('sequelizeClient').models.users
+      const users = app.get('sequelizeClient').models.users
       const { firstName, lastName, email, password } = req.body
-      const passwordHash = await bcrypt.hash(password, bcrypt.genSaltSync(8))
+      const passwordHash = await encryptPassword(password)
       await users.create({ firstName, lastName, email, password: passwordHash })
 
-      const { accessToken } = await a.service('api/authentication').create({
+      const { accessToken } = await app.service('api/authentication').create({
         strategy: 'local',
         email,
         password,
@@ -28,15 +39,15 @@ module.exports = (a) => {
       }
 
       res.redirect(afterLoginRedirect)
-    } catch (e) {
-      next(e)
+    } catch (error) {
+      next(error)
     }
   })
 
-  a.post('/login', async (req, res, next) => {
+  app.post('/login', async (req, res, next) => {
     try {
       const { email, password } = req.body
-      const { accessToken } = await a.service('api/authentication').create({
+      const { accessToken } = await app.service('api/authentication').create({
         strategy: 'local',
         email,
         password,
@@ -53,33 +64,16 @@ module.exports = (a) => {
     }
   })
 
-  a.get(
-    '/jwt',
-    (req, res, next) => {
-      req.authentication = req.session.authentication
-      next()
-    },
-    authenticate('jwt'),
-    (req, res) => {
-      res.send(req.session.authentication.accessToken)
-    }
-  )
+  app.get('/jwt', setAuthenticationFromSession, authenticate('jwt'), (req, res) => {
+    res.send(req.session.authentication.accessToken)
+  })
 
-  a.get(
-    '/currentUser',
-    (req, res, next) => {
-      req.authentication = req.session.authentication
-      next()
-    },
-    authenticate('jwt'),
+  app.get('/currentUser', setAuthenticationFromSession, authenticate('jwt'), (req, res) => {
+    res.send({ currentUser: req.user })
+  })
 
-    (req, res) => {
-      res.send({ currentUser: req.user })
-    }
-  )
-
-  a.get('/logout', (req, res) => {
+  app.get('/logout', (req, res) => {
     delete req.session.authentication
-    res.end('You are now logged out')
+    res.redirect('/')
   })
 }
